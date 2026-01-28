a23d4087b56e5b860bd297f83118d9aa92770fd3
shunw <shunw.li@outlook.com>
2026-01-28 14:45:33 +0800

    complete the 4 finger parameter

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # Purpose: Capture Human Hand for Robot Hand Mimic
+
+## step 1: to detect the hand from the camera or picture
+
+## step 2: to convert the hand marker into the parameter of robot hand
+
+
+# current status
+
+- [x] complete the hand detect
+- [ ] convert the hand marker data into the parameter of robot hand
+    - the work logic will be three steps: 
+    1. check how to convert with one figure (except the thumb)
+    2. check how to do with the 4 fingers spread
+    3. check how to covert the thumb
+    4. check with all the figure angles with different shape, if it still can have a good mimic
+
+### convert with the four fingers
+- dip? to dexhand, this means the whole finger 斜度, this is all the finger mcp (5, 9, 13, 17) to wrist (0), vs the mcp to pip (6, 10, 14, 18) angles
+- mcp? to dexhand, this means how curl finger is
+    - the simple way would be the angle of mcp to pip vs pip to dip
+    - or the way would be the average of two angles: the other angle is pip to dip vs dip to tip
+
+
+
+# dexhand joint commands based as below
+```
+@dataclass
+class JointConfig:
+    """Configuration for a joint including limits"""
+
+    min_angle: float = 0.0
+    max_angle: float = 90.0  # Default for most joints
+
+
+JOINT_CONFIGS = {
+    "th_rot": JointConfig(max_angle=150.0),  # Thumb rotation has extended range
+    "th_mcp": JointConfig(),  # Default 0-90 / # 拇指掌指关节弯曲（0-90度）
+    "th_dip": JointConfig(),  # 拇指远端关节弯曲
+    "ff_spr": JointConfig(max_angle=30.0),  # Finger spread is limited
+    "ff_mcp": JointConfig(),
+    "ff_dip": JointConfig(),
+    "mf_mcp": JointConfig(),
+    "mf_dip": JointConfig(),
+    "rf_mcp": JointConfig(),
+    "rf_dip": JointConfig(),
+    "lf_mcp": JointConfig(),
+    "lf_dip": JointConfig(),
+}
+```
--- src/hand_visual/hand_detector_static.py
+++ src/hand_visual/hand_detector_static.py
@@ -1,6 +1,11 @@
 #@markdown We implemented some functions to visualize the hand landmark detection results. <br/> Run the following cell to activate the functions.
-import mediapipe as mp
 import numpy as np
+
+# STEP 1: Import the necessary modules.
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+import cv2
 
 mp_hands = mp.tasks.vision.HandLandmarksConnections
 mp_drawing = mp.tasks.vision.drawing_utils
@@ -12,59 +17,67 @@ FONT_THICKNESS = 1
 HANDEDNESS_TEXT_COLOR = (88, 205, 54) # vibrant green
 
 def draw_landmarks_on_image(rgb_image, detection_result):
-  hand_landmarks_list = detection_result.hand_landmarks
-  handedness_list = detection_result.handedness
-  annotated_image = cv2.cvtColor(np.copy(rgb_image), cv2.COLOR_RGB2BGR)
+    hand_landmarks_list = detection_result.hand_landmarks
+    handedness_list = detection_result.handedness
+    annotated_image = cv2.cvtColor(np.copy(rgb_image), cv2.COLOR_RGB2BGR)
 
-  # Loop through the detected hands to visualize.
-  for idx in range(len(hand_landmarks_list)):
-    hand_landmarks = hand_landmarks_list[idx]
-    handedness = handedness_list[idx]
+    # Loop through the detected hands to visualize.
+    for idx in range(len(hand_landmarks_list)): # idx should be how many hands be detected
+        hand_landmarks = hand_landmarks_list[idx] # shows the 21 hand mark at once 
+        handedness = handedness_list[idx] # this is to check left or right hand, and the score of it
+        
+        # Draw the hand landmarks.
+        mp_drawing.draw_landmarks(
+            annotated_image,
+            hand_landmarks,
+            mp_hands.HAND_CONNECTIONS,
+            mp_drawing_styles.get_default_hand_landmarks_style(),
+            mp_drawing_styles.get_default_hand_connections_style())
 
-    # Draw the hand landmarks.
-    mp_drawing.draw_landmarks(
-      annotated_image,
-      hand_landmarks,
-      mp_hands.HAND_CONNECTIONS,
-      mp_drawing_styles.get_default_hand_landmarks_style(),
-      mp_drawing_styles.get_default_hand_connections_style())
+        # Get the top left corner of the detected hand's bounding box.
+        height, width, _ = annotated_image.shape
+        x_coordinates = [landmark.x for landmark in hand_landmarks]
+        y_coordinates = [landmark.y for landmark in hand_landmarks]
+        text_x = int(min(x_coordinates) * width)
+        text_y = int(min(y_coordinates) * height) - MARGIN
 
-    # Get the top left corner of the detected hand's bounding box.
-    height, width, _ = annotated_image.shape
-    x_coordinates = [landmark.x for landmark in hand_landmarks]
-    y_coordinates = [landmark.y for landmark in hand_landmarks]
-    text_x = int(min(x_coordinates) * width)
-    text_y = int(min(y_coordinates) * height) - MARGIN
+        # Draw handedness (left or right hand) on the image.
+        cv2.putText(annotated_image, f"{handedness[0].category_name}",
+                                (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
+                                FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
 
-    # Draw handedness (left or right hand) on the image.
-    cv2.putText(annotated_image, f"{handedness[0].category_name}",
-                (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
-                FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
+    return (annotated_image, hand_landmarks_list[0])
 
-  return annotated_image
+from hand_para_convert import calculate_angle_supple, FingerConvert
+def main():
+    # STEP 2: Create an HandLandmarker object.
+    base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
+    options = vision.HandLandmarkerOptions(base_options=base_options,num_hands=2)
+    detector = vision.HandLandmarker.create_from_options(options)
 
-# STEP 1: Import the necessary modules.
-import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
-import cv2
+    # STEP 3: Load the input image.
+    # IMPORTANT: Replace 'hand.png' with the actual path to your hand image.
+    img = 'hand2.png'
+    image = mp.Image.create_from_file(img)
 
-# STEP 2: Create an HandLandmarker object.
-base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
-options = vision.HandLandmarkerOptions(base_options=base_options,
-                                       num_hands=2)
-detector = vision.HandLandmarker.create_from_options(options)
+    # STEP 4: Detect hand landmarks from the input image.
+    detection_result = detector.detect(image)
 
-# STEP 3: Load the input image.
-# IMPORTANT: Replace 'hand.png' with the actual path to your hand image.
-img = 'hand2.png'
-image = mp.Image.create_from_file(img)
+    # STEP 5: Process the classification result. In this case, visualize it.
+    (annotated_image, hand_landmarks) = draw_landmarks_on_image(image.numpy_view(), detection_result)
+    cv2.imshow("Hand Landmark Detector", annotated_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-# STEP 4: Detect hand landmarks from the input image.
-detection_result = detector.detect(image)
+    # STEP 6: check the para convert
+    f = FingerConvert(hand_landmarks)
+    index_angle_mcp = f._get_dexhand_mcp('ff')
+    index_angle_dip = f._get_dexhand_dip('ff')
+    hand_spread = f._get_finger_spr()
+    # 3. Print or display the result
+    print(f"Index Finger Joint Angle: {index_angle_dip:.2f}")
+    print (f"Hand spread angle is {hand_spread}")
 
-# STEP 5: Process the classification result. In this case, visualize it.
-annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
-cv2.imshow("Hand Landmark Detector", annotated_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
